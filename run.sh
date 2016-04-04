a366b68df0fa2fff62e6e0c32eed473742c56825
R0GGER <rogger@spotx.nu>
2016-04-04 17:54:31 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,4 @@
 !/bin/bash
 
-SYSTEM=${SYSTEM:-"all-x64"}
-LANGUAGE=${LANGUAGE:-"enu"}
-PARAMS=${PARAMS:-"/msse /dotnet"}
-SLEEP=${SLEEP:-"48h"}
-
-ctrlc()
-{
-    exit 1   # stop the script
-}
-
-trap 'ctrlc' INT
-
-while true; do
-    cd /app/wsusoffline/sh && ./DownloadUpdates.sh "$SYSTEM" "$LANGUAGE" "$PARAMS"
-    echo "Sleeping for $SLEEP before next download."
-    sleep "$SLEEP"
-done
+cd /app/wsusoffline/sh && ./DownloadUpdates.sh "$SYSTEM" "$LANGUAGE" "$PARAMS"
+cd /app/wsusoffline/sh && ./DownloadUpdates.sh "$OFFICE" "$LANGUAGE" "$PARAMS"
