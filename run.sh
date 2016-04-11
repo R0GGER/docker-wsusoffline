70377c7838ebc0d6522446dc721d47a1dd1acde5
R0GGER <rogger@spotx.nu>
2016-04-11 16:29:16 +0200

    Fix hashdeep

--- run.sh
+++ run.sh
@@ -2,13 +2,16 @@
 
 # temp fix for hashdeep error wsusoffline 10.6.1
 rm -rf /app/wsusoffline/client/md/*
-
+#
 for SYSTEMS in ${SYSTEMS}; do
     for LANGUAGE in ${LANGUAGE}; do
         cd /app/wsusoffline/sh && ./DownloadUpdates.sh ${SYSTEMS} ${LANGUAGE} ${PARAMS}
     done
 done
 
+# temp fix for hashdeep error wsusoffline 10.6.1
+rm -rf /app/wsusoffline/client/md/*
+#
 for OFFICE in ${OFFICE}; do
     for LANGUAGE in ${LANGUAGE}; do
        cd /app/wsusoffline/sh && ./DownloadUpdates.sh ${OFFICE} ${LANGUAGE}
