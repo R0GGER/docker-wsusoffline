ec2a14befa5544093595acb204ea05d5a82612b4
R0GGER <rogger@spotx.nu>
2016-08-05 12:01:22 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
-# temp fix for hashdeep error wsusoffline 10.6.1
-rm -rf /app/wsusoffline/client/md/*
-#
+## SIMLINK
+# ln -s /wsus/run.sh /etc/my_init.d/run.sh
+# ln -s /wsus/run.sh /etc/cron.weekly/run.sh
+
+#WSUS="/wsus"
+#SYSTEMS="all-61 all-63 all-100"
+#OFFICE="o2k13 o2k16"
+#LANGUAGE="nld"
+#PARAMS="/dotnet /msse /wddefs /wle /makeiso"
+#CRON="weekly"
+
+cd $WSUS/ && ./update.sh
+sleep 10
+
 for SYSTEMS in ${SYSTEMS}; do
     for LANGUAGE in ${LANGUAGE}; do
-        cd /app/wsusoffline/sh && ./DownloadUpdates.sh ${SYSTEMS} ${LANGUAGE} ${PARAMS}
+        cd $WSUS/wsusoffline/sh && ./DownloadUpdates.sh ${SYSTEMS} ${LANGUAGE} ${PARAMS}
     done
 done
 
-# temp fix for hashdeep error wsusoffline 10.6.1
-rm -rf /app/wsusoffline/client/md/*
-#
 for OFFICE in ${OFFICE}; do
     for LANGUAGE in ${LANGUAGE}; do
-       cd /app/wsusoffline/sh && ./DownloadUpdates.sh ${OFFICE} ${LANGUAGE}
+       cd $WSUS/wsusoffline/sh && ./DownloadUpdates.sh ${OFFICE} ${LANGUAGE}
     done
 done
+
 
