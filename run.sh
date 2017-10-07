c825f1c4ba379a9c7414f4b893ba44712a5e0c09
R0GGER <rogger@spotx.nu>
2017-10-07 20:49:57 +0200

    Add: sleep

--- run.sh
+++ run.sh
@@ -1,18 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-cd /wsus/ && ./update.sh
+SLEEP=${SLEEP:-"$SLEEP"}
+
+ctrlc()
+{
+    exit 1   # stop the script
+}
+
+trap 'ctrlc' INT
+
+while true; do
+
+cd /wsus && ./update.sh
 sleep 10
 
-for SYSTEMS in ${SYSTEMS}; do
+  for SYSTEMS in ${SYSTEMS}; do
     for LANGUAGE in ${LANGUAGE}; do
-        cd /wsus/wsusoffline/sh && ./DownloadUpdates.sh ${SYSTEMS} ${LANGUAGE} ${PARAMS}
+        cd /wsus/wsusoffline/sh && ./download-updates.bash ${SYSTEMS} ${LANGUAGE} ${PARAMS}
     done
-done
+  done
 
-for OFFICE in ${OFFICE}; do
+  for OFFICE in ${OFFICE}; do
     for LANGUAGE in ${LANGUAGE}; do
-       cd /wsus/wsusoffline/sh && ./DownloadUpdates.sh ${OFFICE} ${LANGUAGE}
+        cd /wsus/wsusoffline/sh && ./download-updates.bash ${OFFICE} ${LANGUAGE}
     done
+  done
+  echo "Sleeping for $SLEEP before next download."
+  sleep "$SLEEP"
 done
-
-
